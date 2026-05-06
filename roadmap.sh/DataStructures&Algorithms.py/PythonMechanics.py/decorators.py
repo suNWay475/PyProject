ba7e3b63dab66c1e411b8e800128cabdc2083685
Vitalii Bukshan <sway6432@gmail.com>
2026-05-06 20:55:23 +0300

    update for decorators

--- roadmap.sh/DataStructures&Algorithms.py/PythonMechanics.py/decorators.py
+++ roadmap.sh/DataStructures&Algorithms.py/PythonMechanics.py/decorators.py
@@ -1,13 +1,30 @@
-from functools import wraps
+# from functools import wraps
 
-def decorator(func):
-    @wraps(func)  # запамятай своє ім'я
-    def wrapper(*args, **kwargs): # приймає всі аргументи які ми задамо
-        return func(*args, **kwargs)
-    return wrapper
+# def decorator(func):
+#     @wraps(func)  # запамятай своє ім'я
+#     def wrapper(*args, **kwargs): # приймає всі аргументи які ми задамо
+#         return func(*args, **kwargs)
+#     return wrapper
 
-@decorator
-def add(a, b):
-    return a + b
+# @decorator #або decorator(add)
+# def add(a, b):
+#     return a + b
 
-print(add.__name__)  # add  ✅ — ім'я збереглось!
+# print(add.__name__)  # add  ✅ — ім'я збереглось!
+
+
+
+def pretty_sumab(func):    # приймає функцію sumab
+    def inner(a, b):       # обгортка з тими ж аргументами
+        print(str(a) + " + " + str(b) + " is ", end="")  
+        # виводить "5 + 3 is " (end="" — без переносу рядка)
+        return func(a, b)  # викликає оригінальний sumab
+    return inner
+@pretty_sumab
+def sumab(a, b):
+    summed = a + b
+    print(summed)  # виводить результат
+
+
+if __name__ == "__main__": # запустится якщо тільки файл головний !
+    sumab(5,3)
